9199bad6313727c7ca296dfe072c88f6de0dd6d2
bhanurx100 <bhanu100rx@gmail.com>
2026-07-18 00:12:17 +0530

    chore: remove legacy UI files

--- src/shared/navigation/index.ts
+++
@@ -1,4 +0,0 @@
-export { AppShell }       from "./AppShell";
-export { MobileHeader }   from "./MobileHeader";
-export { MobileDock }     from "./MobileDock";
-export { PageContainer }  from "./PageContainer";
